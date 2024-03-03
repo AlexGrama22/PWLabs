6353b542e95ec70d94aeb529f6a52b93b6480a9c
Alexandru Grama <a.grama@webdev.global>
2024-03-03 19:40:04 +0200

    masscot movement

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    setTimeout(() => {
-        const mascot = document.getElementById('mascot');
-        mascot.style.opacity = 1;
-    }, 5000); // 5-second delay
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     setTimeout(() => {
+//         const mascot = document.getElementById('mascot');
+//         mascot.style.animationPlayState = 'running'; 
+//     }, 5000); 
 
+//     const mascot = document.getElementById('mascot');
+//     mascot.addEventListener('mouseover', () => {
+//         alert('Welcome to XYX Construct! We are happy to help you. If you need assistance, call us at: +37369833910');
+//     });
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
     const mascot = document.getElementById('mascot');
+    mascot.style.animation = `slideIn 2s ease-out 5s forwards, moveLeftRight 2s ease-in-out infinite alternate 7s`;
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('mascot-message');
+    messageDiv.innerHTML = 'Welcome to XYX Construct! We are happy to help you. If you need assistance,';
+
+    const ctaDiv = document.createElement('div');
+    ctaDiv.classList.add('mascot-cta');
+    ctaDiv.textContent = 'Call us: +37369-833-910';
+    ctaDiv.addEventListener('click', () => {
+        window.location.href = 'tel:+37369-833-910';
+    });
+
+    messageDiv.appendChild(ctaDiv);
+
     mascot.addEventListener('mouseover', () => {
-        alert('Welcome! How can we help you with your construction needs?');
+        mascot.appendChild(messageDiv); 
+        messageDiv.style.display = 'block';
+    });
+
+    mascot.addEventListener('mouseout', () => {
+        messageDiv.style.display = 'none'; 
+        mascot.removeChild(messageDiv); 
     });
 });
